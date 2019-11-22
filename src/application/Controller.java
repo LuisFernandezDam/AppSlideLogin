c77e054849399ba792e1072b6bbe53ddb3748aec
Luis <luisfn1990@gmail.com>
2019-11-22 08:54:03 +0100

    Cambios Home

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -1,8 +1,16 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.animation.FadeTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class Controller {
@@ -19,12 +27,34 @@ public class Controller {
 	@FXML
 	private Pane pane4;
 
+	@FXML
+	private void newPane(ActionEvent event) {
+
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Home.fxml"));
+			Parent root1;
+			root1 = (Parent) fxmlLoader.load();
+			Stage stage = new Stage();
+			// set what you want on your stage
+			stage.initModality(Modality.APPLICATION_MODAL);
+			stage.setTitle("Report Page");
+			stage.setScene(new Scene(root1));
+			stage.setResizable(false);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	public void initialize() {
 
-		/*pane1.setStyle("-fx-background-image: url('img1.jpg')");
-		pane2.setStyle("-fx-background-image: url('img2.jpg')");
-		pane3.setStyle("-fx-background-image: url('img3.jpg')");
-		pane4.setStyle("-fx-background-image: url('img4.jpg')");*/
+		/*
+		 * pane1.setStyle("-fx-background-image: url('img1.jpg')");
+		 * pane2.setStyle("-fx-background-image: url('img2.jpg')");
+		 * pane3.setStyle("-fx-background-image: url('img3.jpg')");
+		 * pane4.setStyle("-fx-background-image: url('img4.jpg')");
+		 */
 
 		backgroundAnimation();
 
